576b61cba20a7165f3baade2a5faa3228245025f
Andrew Thorburn <andrew.thorburn@snyk.io>
2022-05-30 16:25:14 +0200

    Initial commit

---
+++ src/main/java/io/snyk/oldinsecuredemo/OldInsecureDemoApplication.java
@@ -0,0 +1,13 @@
+package io.snyk.oldinsecuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldInsecureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldInsecureDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/snyk/oldinsecuredemo/OldInsecureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.snyk.oldinsecuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldInsecureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
